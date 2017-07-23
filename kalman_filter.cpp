e81553023ce06851d15010311a1b2c09485ea528
Kendrick Amezquita <kendrick1501@gmail.com>
2017-07-23 20:47:15 +0800

    Update kalman_filter.cpp

--- kalman_filter.cpp
+++ kalman_filter.cpp
@@ -11,14 +11,14 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Predict() {
 
-    x_ = F_ * x_;
+	x_ = F_ * x_;
 	MatrixXd Ft = F_.transpose();
 	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
 
-    VectorXd z_pred = H_ * x_;
+	VectorXd z_pred = H_ * x_;
 	VectorXd y = z - z_pred;
 	MatrixXd Ht = H_.transpose();
 	MatrixXd S = H_ * P_ * Ht + R_;
@@ -35,17 +35,17 @@ void KalmanFilter::Update(const VectorXd &z) {
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
 
-    float px = x_(0);
-    float py = x_(1);
-    float vx = x_(2);
-    float vy = x_(3);
+	float px = x_(0);
+	float py = x_(1);
+	float vx = x_(2);
+	float vy = x_(3);
 
-    float rho = sqrt(px*px+py*py);
-    VectorXd h_x;
-    h_x = VectorXd(3);
-    h_x(0) = rho;
-    h_x(1) = atan2(py,px);
-    h_x(2) = (px*vx+py*vy)/rho;
+	float rho = sqrt(px*px+py*py);
+	VectorXd h_x;
+	h_x = VectorXd(3);
+	h_x(0) = rho;
+	h_x(1) = atan2(py,px);
+	h_x(2) = (px*vx+py*vy)/rho;
 
 	VectorXd y = z - h_x;
 	MatrixXd Ht = H_.transpose();
@@ -55,12 +55,12 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 	MatrixXd K = PHt * Si;
 
 	if (y(1) > M_PI) {
-        y(1) -= 2*M_PI;
-    } else if (y(1) < -M_PI) {
-        y(1) += 2*M_PI;
-    }
+		y(1) -= 2*M_PI;
+	} else if (y(1) < -M_PI) {
+		y(1) += 2*M_PI;
+	}
 
-    //new estimate
+	//new estimate
 	x_ = x_ + (K * y);
 	long x_size = x_.size();
 	MatrixXd I = MatrixXd::Identity(x_size, x_size);
